abfdc586f16b4388892b50158279ccd7ee936f0a
adm <etomojapo4ta@gmail.com>
2018-11-08 00:21:42 +0200

    fix for formmodule

--- ClientApp/src/app/components/components.module.ts
+++ ClientApp/src/app/components/components.module.ts
@@ -1,9 +1,10 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [VehicleFormComponent],
   exports: [VehicleFormComponent]
 })
